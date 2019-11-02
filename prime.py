59bb1bcb89f029c06e7815b5e688b448585e444c
wonderme88 <wonderme.88@gmail.com>
2019-11-02 06:45:59 -0400

    updated prime no

--- prime.py
+++ prime.py
@@ -1,13 +1,17 @@
 n=1,2,3,4,5,6
 
 #p=11
-p=input("Enter a no to check=")
-print(type(p))
-for i in range(2,p):	
+p=int(input("Enter any no to check="))
+
+if p>1:
+
+	for i in range(2,p):	
 	
-	if p%i==0:
-		print ("Not Prime")
-		break
+		if p%i==0:
+			print (p,' is Not a Prime')
+			break
 	else:
-		print ("Prime")
-		break
+		print (p,'is a Prime Number')
+			
+else:
+	print(p," is not a prime number")
